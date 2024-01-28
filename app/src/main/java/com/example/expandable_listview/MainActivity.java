4c7d7de7a7e12d8e7d5165beede4708ae6ea8ddd
Md Mehedi Hasan <145797601+mehediinf@users.noreply.github.com>
2024-01-29 03:54:46 +0600

    Fourth Commit Expandable_Listview

--- app/src/main/java/com/example/expandable_listview/MainActivity.java
+++ app/src/main/java/com/example/expandable_listview/MainActivity.java
@@ -85,6 +85,157 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+
+//.....................  preparingListData ...................
+    private void prepareListData(){
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        //Adding child data
+
+        listDataHeader.add("1. Overview");
+        listDataHeader.add("2. Environment setup");
+        listDataHeader.add("3. Program structure");
+        listDataHeader.add("4. printf scanf");
+        listDataHeader.add("5. Keywords");
+        listDataHeader.add("6. Data types");
+        listDataHeader.add("7. Variable");
+        listDataHeader.add("8. Operator");
+        listDataHeader.add("9. Comments");
+        listDataHeader.add("10. Math.h");
+        listDataHeader.add("11. Control Statements");
+        listDataHeader.add("12. Array");
+        listDataHeader.add("13. String");
+        listDataHeader.add("14. Function");
+        listDataHeader.add("15. Pointer");
+        listDataHeader.add("16. Structure");
+        listDataHeader.add("17. Files");
+
+// Adding child data to the overview
+        List<String> overView = new ArrayList<>();
+        overView.add("1.1 What is C language");
+        overView.add("1.2 History of C");
+        overView.add("1.3 Features of C");
+        overView.add("1.4 Advantages of C");
+
+// Adding child data to the environmentSetup
+        List<String> environmentSetup = new ArrayList<>();
+        environmentSetup.add("2.1 How to install Codeblock");
+
+// Adding child data to the programStructure
+        List<String> programStructure = new ArrayList<>();
+        programStructure.add("3.1 Program Structure");
+
+
+// Adding child data to the printfScanf
+        List<String> printfScanf = new ArrayList<>();
+        printfScanf.add("4.1 printf scanf in C");
+
+// Adding child data to the keywords
+        List<String> keywords = new ArrayList<>();
+        keywords.add("5.1 keywords in C");
+
+// Adding child data to the dataType
+        List<String> dataType = new ArrayList<>();
+        dataType.add("6.1 Data type in C");
+
+
+// Adding child data to the variables
+        List<String> variables = new ArrayList<>();
+        variables.add("7.1 Variables in C");
+        variables.add("7.2 Variables Test");
+
+// Adding child data to the operator
+        List<String> operator = new ArrayList<>();
+        operator.add("8.1 What is operator");
+
+
+// Adding child data to the comment group
+        List<String> comment = new ArrayList<>();
+        comment.add("9.1 Comments in C");
+
+// Adding child data to the math group
+        List<String> math = new ArrayList<>();
+        math.add("10.1 Math Function in C");
+
+// Adding child data to the controlStatements group
+        List<String> controlStatements = new ArrayList<>();
+        controlStatements.add("11.1 if-else");
+        controlStatements.add("11.2 Loop");
+
+// Adding child data to the array group
+        List<String> array = new ArrayList<>();
+        array.add("12.1 One dimensional Array");
+        array.add("12.2 Two dimensional Array");
+
+
+// Adding child data to the string group
+        List<String> string = new ArrayList<>();
+        string.add("13.1 String in C");
+
+
+
+// Adding child data to the function group
+        List<String> function = new ArrayList<>();
+        function.add("14.1 Function in C");
+
+
+// Adding child data to the pointer group
+        List<String> pointer = new ArrayList<>();
+        pointer.add("15.1 Pointer in C");
+        pointer.add("15.2 Pointer Test");
+
+
+// Adding child data to the structure_union group
+        List<String> structure_union = new ArrayList<>();
+        structure_union.add("16.1 Structure union in C");
+        structure_union.add("16.2 Structure Test");
+
+
+// Adding child data to the files group
+        List<String> files = new ArrayList<>();
+        files.add("17.1 File Handling in C");
+        files.add("17.2 fprintf() fscanf()");
+
+
+        //Header , Chile Data
+
+        listDataChild.put(listDataHeader.get(0),overView);
+        listDataChild.put(listDataHeader.get(1),environmentSetup);
+        listDataChild.put(listDataHeader.get(2),programStructure);
+        listDataChild.put(listDataHeader.get(3),printfScanf);
+        listDataChild.put(listDataHeader.get(4),keywords);
+        listDataChild.put(listDataHeader.get(5),dataType);
+        listDataChild.put(listDataHeader.get(6),variables);
+        listDataChild.put(listDataHeader.get(7),operator);
+        listDataChild.put(listDataHeader.get(8),comment);
+        listDataChild.put(listDataHeader.get(9),math);
+        listDataChild.put(listDataHeader.get(10),controlStatements);
+        listDataChild.put(listDataHeader.get(11),array);
+        listDataChild.put(listDataHeader.get(12),string);
+        listDataChild.put(listDataHeader.get(13),function);
+        listDataChild.put(listDataHeader.get(14),pointer);
+        listDataChild.put(listDataHeader.get(15),structure_union);
+        listDataChild.put(listDataHeader.get(16),files);
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+//.........................................................
+
+    /*   This is a use in by 1 chile use in header
+
+
     public void prepareListData(){
 
         String[] headerString = getResources().getStringArray(R.array.network_abbreviation_list_header);
@@ -109,6 +260,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+
+    */
 
 
 
